cfb4123d1d50f5c1e0ec9e087380862e11955c1c
larsommen <lars@ommen.it>
2019-07-23 17:24:05 +0200

    added python script to remove error flags every hour

---
+++ remoeErrorFlags.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import commands
+
+
+
+existsTempError = os.path.isfile('/home/pi/winecellar/tmpdata/errorTemp.test')
+existsHumidError = os.path.isfile('/home/pi/winecellar/tmpdata/errorHumid.test')
+
+if existsHumidError:
+	subprocess.call(['rm', '/home/pi/winecellar/tmpdata/errorHumid.test'])
+
+if existsTempError:
+        subprocess.call(['rm', '/home/pi/winecellar/tmpdata/errorTemp.test'])	
